33e456c3f3515d9bb0ed0ac1a6cb45c69dade5c0
Cullen Middleton <culmid@gmail.com>
2022-05-25 17:06:21 +0200

    Constructors screen

--- screens/ConstructorsScreen/index.js
+++ screens/ConstructorsScreen/index.js
@@ -1,10 +1,65 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
 import style from "./style";
+import countries from "../../assets/countries.json";
+import Flag from "react-native-flags";
 
 export default function ConstructorsScreen() {
+  const [constructors, setConstructors] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const renderConstructors = ({ item }) => {
+    const country = countries.find(
+      (country) => country.Nationality == item.nationality
+    );
+
+    console.log(item);
+    return (
+      <View style={style.constructorContainer}>
+        <View style={style.constructorHeader}>
+          <Text style={style.constructorName}>{item.name}</Text>
+          {country && <Flag code={country.CCA2} size={24} />}
+        </View>
+        <Text style={style.nationality}>{item.nationality}</Text>
+      </View>
+    );
+  };
+
+  useEffect(() => {
+    async function doFetchConstructors() {
+      try {
+        const data = await fetch("https://ergast.com/api/f1/constructors.json");
+        const json = await data.json();
+
+        setConstructors(json.MRData.ConstructorTable.Constructors);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    setTimeout(() => {
+      // For testing - TODO:Remove
+      doFetchConstructors();
+    }, 5000);
+  }, []);
+
   return (
-    <View>
-      <Text>ConstructorsScreen</Text>
+    <View style={style.constructorsContainer}>
+      {isLoading && !isError && (
+        <ActivityIndicator size="large" color="#ffa500" />
+      )}
+      {isError && <Text>Error, Please Refresh the App</Text>}
+      {!isLoading && !isError && (
+        <FlatList
+          data={constructors}
+          renderItem={renderConstructors}
+          keyExtractor={(constructor) => constructor.constructorId}
+          extraData={constructors}
+          style={style.constructorsList}
+        />
+      )}
     </View>
   );
 }
--- screens/ConstructorsScreen/style.js
+++ screens/ConstructorsScreen/style.js
@@ -1,5 +1,34 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  //
+  constructorsContainer: {
+    flex: 1,
+    backgroundColor: "#ff00ff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  constructorsList: {
+    width: "100%",
+    backgroundColor: "blue",
+  },
+  constructorContainer: {
+    backgroundColor: "green",
+    marginBottom: 2,
+    borderRadius: 10,
+    padding: 5,
+  },
+  constructorHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  constructorName: {
+    fontWeight: "700",
+    fontSize: 20,
+  },
+  nationality: {
+    fontWeight: "300",
+    fontSize: 18,
+  },
 });
