204ca9a1495ff492e241596bf1a1d2990957fbb3
Cullen Middleton <culmid@gmail.com>
2022-05-25 16:14:18 +0200

    Drivers screen

--- screens/DriversScreen/index.js
+++ screens/DriversScreen/index.js
@@ -1,10 +1,75 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
 import style from "./style";
+import countries from "../../assets/countries.json";
+import Flag from "react-native-flags";
+
+console.log(countries);
 
 export default function DriversScreen() {
+  const [drivers, setDrivers] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const renderDriver = ({ item }) => {
+    const country = countries.find(
+      (country) => country.Nationality == item.nationality
+    );
+    console.log(country);
+    console.log(item);
+    return (
+      <View style={style.driverContainer}>
+        <View style={style.driverHeader}>
+          <Text style={style.driverHeaderText}>
+            {item.givenName} {item.familyName}
+          </Text>
+          <View style={style.driverHeaderDecoration}>
+            <Text style={style.driverHeaderText}>
+              {item.code} {item.permanentNumber}{" "}
+            </Text>
+            {country && <Flag code={country.CCA2} size={24} />}
+          </View>
+        </View>
+        <Text style={style.dob}>{item.dateOfBirth}</Text>
+        <Text style={style.nationality}>{item.nationality}</Text>
+      </View>
+    );
+  };
+
+  useEffect(() => {
+    async function doFetchDrivers() {
+      try {
+        const data = await fetch("https://ergast.com/api/f1/drivers.json");
+        const json = await data.json();
+
+        setDrivers(json.MRData.DriverTable.Drivers);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    setTimeout(() => {
+      // For testing - TODO:Remove
+      doFetchDrivers();
+    }, 5000);
+  }, []);
+
   return (
-    <View>
-      <Text>DriversScreen</Text>
+    <View style={style.driversContainer}>
+      {isLoading && !isError && (
+        <ActivityIndicator size="large" color="#ffa500" />
+      )}
+      {isError && <Text>Error, Please Refresh the App</Text>}
+      {!isLoading && !isError && (
+        <FlatList
+          data={drivers}
+          renderItem={renderDriver}
+          keyExtractor={(driver) => driver.driverId}
+          extraData={drivers}
+          style={style.driversList}
+        />
+      )}
     </View>
   );
 }
--- screens/DriversScreen/style.js
+++ screens/DriversScreen/style.js
@@ -1,5 +1,45 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  //
+  driversContainer: {
+    flex: 1,
+    backgroundColor: "#ff00ff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  loadingSpinner: {
+    width: 500,
+    height: 500,
+  },
+  driversList: {
+    width: "100%",
+    backgroundColor: "blue",
+  },
+  driverContainer: {
+    backgroundColor: "green",
+    marginBottom: 2,
+    borderRadius: 10,
+    padding: 5,
+  },
+  driverHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  driverHeaderDecoration: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  driverHeaderText: {
+    fontWeight: "700",
+    fontSize: 20,
+  },
+  dob: {
+    fontStyle: "italic",
+  },
+  nationality: {
+    fontWeight: "300",
+    fontSize: 18,
+  },
 });
